e93fe235ed790af5f637accec10319328bb12f69
hajaratali <hazarathalibibijan63@gmail.com>
2025-03-18 01:08:46 +0530

    Basic : problems on tress

--- Road_map/src/Trees/BinaryTree.java
+++ Road_map/src/Trees/BinaryTree.java
@@ -1,6 +1,9 @@
 package Trees;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class TreeNode{
 
     int data;
@@ -15,12 +18,36 @@ class TreeNode{
 }
 public class BinaryTree {
     TreeNode root;
-
+    public static void InOrder(TreeNode root){
+        if(root == null) return;
+        InOrder(root.left);
+        System.out.print(root.data+" ");
+        InOrder(root.right);
+    }
     public static void PreOrder(TreeNode root){
         if(root == null) return;
         System.out.print(root.data+" ");
         PreOrder(root.left);
         PreOrder(root.right);
+    }
+
+    public static void PostOrder(TreeNode root){
+        if(root == null) return;
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void levelOrder(TreeNode root){
+        if(root == null) return;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            System.out.print(node.data+" ");
+            if(node.left != null) queue.add(node.left);
+            if(node.right != null) queue.add(node.right);
+
+        }
     }
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
@@ -35,5 +62,11 @@ public class BinaryTree {
 
         System.out.println("Preorder traversal of binary tree is ");
         PreOrder(tree.root);
+        System.out.println("\nInorder traversal of binary tree is ");
+        InOrder(tree.root);
+        System.out.println("\nPostorder traversal of binary tree is ");
+        PostOrder(tree.root);
+        System.out.println("\nLevel order traversal of binary tree is ");
+        levelOrder(tree.root);
     }
 }
